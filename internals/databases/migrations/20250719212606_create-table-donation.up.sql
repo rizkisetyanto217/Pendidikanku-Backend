9b2ddd33744c95522cae11b56939acb2cc0948b1
Rizki Setyanto <rizkisetyanto217@gmail.com>
2025-07-20 08:52:06 +0700

    payment_midtrans (1Ne)

--- internals/databases/migrations/20250719212606_create-table-donation.up.sql
+++ internals/databases/migrations/20250719212606_create-table-donation.up.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS donations (
     donation_amount INTEGER NOT NULL CHECK (donation_amount > 0), 
     donation_message TEXT, 
     donation_status VARCHAR(20) NOT NULL DEFAULT 'pending' CHECK (
-        donation_status IN ('pending', 'paid', 'expired', 'canceled')
+        donation_status IN ('pending', 'paid', 'expired', 'canceled', 'completed')  -- Menambahkan 'completed'
     ), 
     donation_order_id VARCHAR(100) NOT NULL UNIQUE CHECK (
         char_length(donation_order_id) <= 100
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS donations (
     donation_payment_gateway VARCHAR(50) DEFAULT 'midtrans',
     donation_payment_method VARCHAR,
     donation_paid_at TIMESTAMP, 
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,                       -- Timestamp pembuatan data
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,                       
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP 
 );
